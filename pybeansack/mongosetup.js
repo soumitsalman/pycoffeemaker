5c96cea66d570f0ecdb717b192c131283a3b1ab7
Soumit Salman Rahman <147952840+soumitsalman@users.noreply.github.com>
2024-08-14 02:23:27 +0000

    updating beansack indexes

--- pybeansack/mongosetup.js
+++ pybeansack/mongosetup.js
@@ -24,29 +24,10 @@ db.runCommand(
 
 db.beans.createIndex(
   {
-      categories: 1
+      url: 1
   },
   {
-      name: "beans_categories_search"
-  }
-);
-
-db.beans.createIndex(
-  {
-      tags: 1
-  },
-  {
-      name: "beans_tags_search"
-  }
-);
-
-db.beans.createIndex(
-  {
-      trend_score: -1,
-      updated: -1
-  },
-  {
-      name: "beans_trend_score_and_latest"
+      name: "beans_url"
   }
 );
 
@@ -55,6 +36,116 @@ db.beans.createIndex(
       cluster_id: 1
   },
   {
-      name: "beans_cluster_id_search"
+      name: "beans_cluster_id"
+  }
+);
+
+db.beans.createIndex(
+  {
+      categories: 1
+  },
+  {
+      name: "beans_categories"
+  }
+);
+
+db.beans.createIndex(
+  {
+      tags: 1
+  },
+  {
+      name: "beans_tags"
+  }
+);
+
+db.beans.createIndex(
+  {
+      kind: 1
+  },
+  {
+      name: "beans_kind"
+  }
+);
+
+db.beans.createIndex(
+  {
+      created: -1
+  },
+  {
+      name: "beans_newest"
+  }
+);
+
+db.beans.createIndex(
+  {
+      updated: -1
+  },
+  {
+      name: "beans_latest"
+  }
+);
+
+db.beans.createIndex(
+  {
+      trend_score: -1
+  },
+  {
+      name: "beans_trending"
+  }
+);
+
+db.beans.createIndex(
+  {
+      created: -1,
+      trend_score: -1
+  },
+  {
+      name: "beans_newest_and_trending"
+  }
+);
+
+db.beans.createIndex(
+  {
+      latest: -1,
+      trend_score: -1
+  },
+  {
+      name: "beans_latest_and_trending"
+  }
+);
+
+db.beans.createIndex(
+  {
+      categories: 1,
+      kind: 1,
+      trend_score: -1
+  },
+  {
+      name: "beans_categories_kind_and_trending"
+  }
+);
+
+db.beans.createIndex(
+  {
+      categories: 1,
+      kind: 1,
+      created: -1,
+      trend_score: -1
+  },
+  {
+      name: "beans_categories_kind_newest_and_trending"
+  }
+);
+
+db.beans.createIndex(
+  {
+      categories: "text",
+      tags: "text",
+      title: "text",
+      summary: "text",
+      highlights: "text"
+  },
+  {
+      name: "beans_text_search"
   }
 );
