2ed127d03615512e7a451752a1b03fbda1d80310
Soumit Salman Rahman <soumitsr@gmail.com>
2025-11-07 17:22:17 -0500

    updating backup

--- factory/backup.sh
+++ factory/backup.sh
@@ -4,5 +4,5 @@ S3_BUCKET=""
 PG_CONNECTION_STRING=""
 LOCAL_DATAPATH=""
 pg_dump --no-owner --no-acl --data-only -Fc $PG_CONNECTION_STRING > "$LOCAL_DATAPATH/catalog/backups/beansackdb_$(date +%Y%m%d_%H%M%S).dump"
-~/go/bin/s5cmd --credentials-file=$S3_CREDENTIALS --endpoint-url=$S3_ENDPOINT sync $LOCAL_DATAPATH/catalog/backups/* s3://$S3_BUCKET/catalog/backups/
-~/go/bin/s5cmd --credentials-file=$S3_CREDENTIALS --endpoint-url=$S3_ENDPOINT sync $LOCAL_DATAPATH/storage/* s3://$S3_BUCKET/storage/
+~/go/bin/s5cmd --credentials-file=$S3_CREDENTIALS --endpoint-url=$S3_ENDPOINT cp --no-clobber $LOCAL_DATAPATH/catalog/backups/* s3://$S3_BUCKET/catalog/backups/
+~/go/bin/s5cmd --credentials-file=$S3_CREDENTIALS --endpoint-url=$S3_ENDPOINT cp --no-clobber $LOCAL_DATAPATH/storage/* s3://$S3_BUCKET/storage/
