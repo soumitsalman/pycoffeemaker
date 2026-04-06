d02e2c5d6f093980b7c10afeb3db4f9cdd0aa452
Soumit Salman Rahman <soumitsr@gmail.com>
2026-04-06 10:53:37 -0400

    add hydration functionality to state caches in migrate script

--- factory/migrate.py
+++ factory/migrate.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 import os
 import logging
 import sys
@@ -95,14 +96,57 @@ def migrate(from_db: str, to_db: str, batch_size: int, window: int, *requested_i
     from_db_instance.close()
     to_db_instance.close()    
 
+def hydrate(from_db, states_cache, classifier_cache, batch_size):
+    from coffeemaker.orchestrators.statemachines import StateMachine
+    from pybeansack import SimpleVectorDB
+
+    db = db_instance(from_db)
+    states = StateMachine(states_cache, {"beans": K_URL, "publishers": K_BASE_URL})
+    classifications = SimpleVectorDB(classifier_cache, {"beans": K_URL})
+
+    if False:
+        offset = 0
+        with tqdm(desc="Hydrating State Caches", unit="publishers") as pbar:
+            while pubs := db.query_publishers(
+                conditions=["(rss_feed IS NOT NULL) OR (favicon IS NOT NULL) OR (site_name IS NOT NULL) OR (description IS NOT NULL)"],
+                limit=batch_size, offset=offset,
+            ):
+                publisher_states = [pub.model_dump(exclude_none=True) for pub in pubs]
+                for state in ["collected"]:
+                    states.set("publishers", state, publisher_states)
+                offset += len(pubs)
+                pbar.update(len(pubs))
+
+    
+    if True:
+        # offset = 139264+24576
+        with tqdm(desc="Hydrating State Caches", unit="beans") as pbar:
+            while beans := db.query_latest_beans(
+                conditions=["gist IS NOT NULL", "embedding IS NOT NULL"],
+                limit=batch_size, offset=offset,
+                columns=[K_URL, K_EMBEDDING, K_COLLECTED]
+            ):
+                with ThreadPoolExecutor() as executor:
+                    bean_states = [bean.model_dump(include={K_URL, K_COLLECTED}) for bean in beans]
+                    executor.map(lambda state: states.set("beans", state, bean_states), ["collected", "embedded", "extracted", "digested"])
+                    executor.submit(classifications.store, "beans", [bean.model_dump(include={K_URL, K_EMBEDDING}) for bean in beans])
+                offset += len(beans)
+                pbar.update(len(beans))
+        classifications.optimize()
+
+
 import argparse
 parser = argparse.ArgumentParser(description="Setup coffeemaker and beansack")
+parser.add_argument('--hydrate', action='store_true', help='Hydrate state caches instead of doing a full migration')
 parser.add_argument('--from_db', type=str, help='Type of database to create')
 parser.add_argument('--to_db', type=str, help='Update the lancedb')
+parser.add_argument('--states_cache', type=str, help='State cache path for hydration')
+parser.add_argument('--classifier_cache', type=str, help='Classifier cache path for hydration')
 parser.add_argument('--batch_size', type=int, default=2048, help='Batch size for migration')
 parser.add_argument("--window", type=int, default=0, help="Window size for migration")
 parser.add_argument('--items', type=str, nargs='*', help='Items to migrate (beans, publishers, chatters)')
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    migrate(args.from_db, args.to_db, args.batch_size, args.window, *(args.items or []))
+    if args.hydrate: hydrate(args.from_db, args.states_cache, args.classifier_cache, args.batch_size)
+    else: migrate(args.from_db, args.to_db, args.batch_size, args.window, *(args.items or []))
