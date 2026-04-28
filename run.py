f5c8d92b01db4ae77a1a17c0213b13d3580b664f
Soumit Salman Rahman <soumitsr@gmail.com>
2026-04-28 17:26:23 -0400

    Update classification cache path in run.py from ".test/clsstore" to ".cache/clsstore"

--- run.py
+++ run.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
         
         cls_cache = ClassificationCache(
             # TODO: change this later
-            ".test/clsstore", 
+            ".cache/clsstore", 
             table_settings={
                 BEANS: {"id_key": K_URL, "distance_func": "l2"},
                 "categories": {"id_key": "category", "distance_func": "cosine"},
