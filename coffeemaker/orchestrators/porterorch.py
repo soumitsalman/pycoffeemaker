f1cd820389d66159f2f61dbdc829b6eaaf03fde3
Soumit Salman Rahman <soumitsr@gmail.com>
2026-05-04 16:48:31 +0000

    adding opt back to porter

--- coffeemaker/orchestrators/porterorch.py
+++ coffeemaker/orchestrators/porterorch.py
@@ -96,5 +96,6 @@ class Porter:
             self.cache.set(CHATTERS, "beansacked", ids)
             total_ported += count
         
+        self.optimize()
         log.info("hydration complete", extra={"source": "beansack", "num_items": total_ported})
         return total_ported
