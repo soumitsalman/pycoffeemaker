922c3f23c3cbf50314d4d124be5eec804ba5ed55
Soumit Salman Rahman <soumitsr@gmail.com>
2026-06-27 15:12:15 -0400

    minor change in env vars and digestor settings

--- factory/thundercompute-s6-tasks.sh
+++ factory/thundercompute-s6-tasks.sh
@@ -85,8 +85,6 @@ fi
         export VLLM_MAX_NUM_BATCHED_TOKENS=98304
         export VLLM_MAX_NUM_SEQS=256
         export VLLM_GPU_MEMORY_UTILIZATION=0.95
-        export VLLM_ENABLE_V1_MULTIPROCESSING=0
-        export DIGESTOR_VLLM_USE_V2_MODEL_RUNNER=0
         cd '$WORKDIR'
         echo '=== [S6 BOOT $(date -u +%Y-%m-%dT%H:%M:%SZ)] ==='
         exec bash '$SCRIPT' $ARGS_QUOTED
