1c47fa5a1862b85e8e58011ea71f6ef005f01807
Soumit Salman Rahman <soumitsr@gmail.com>
2026-07-21 15:19:23 -0400

    fixing tags creation issue

--- workers/tags.py
+++ workers/tags.py
@@ -1,7 +1,14 @@
 from nlp import valid_tags
 
 
-def merge_tags(*tag_lists) -> list[str]:
+def merge_tags(*tag_values) -> list[str]:
     return list(
-        set(valid_tags(item for tag_list in tag_lists if tag_list for item in tag_list))
+        set(
+            valid_tags(
+                item
+                for value in tag_values
+                if value
+                for item in ([value] if isinstance(value, str) else value)
+            )
+        )
     )
